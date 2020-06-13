3f19c8d68181be9a7a38fda63319ead3bb14f43d
chetanpawar19 <chetanpawar19@outlook.com>
2020-06-14 04:18:41 +0530

    first commit demo project

---
+++ src/pkg1/NewTest.java
@@ -0,0 +1,18 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NewTest {
+	public static Logger log = LogManager.getLogger(NewTest.class.getName());;
+
+	@Test
+	public void f() {
+			log.info("Opening google.co.in");
+			log.info("Window maximized");
+			log.info("Window title:"+driver.getTitle());
+			log.info("Current URL:"+driver.getCurrentUrl());
+	}
+}
